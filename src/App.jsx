c8214375e161d5b704128225d0fc532bec494345
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-03-28 02:16:48 +0530

    foldder structure update

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,81 @@
-import Player from "./components/Player.jsx";
-import GameBoard from "./components/GameBoard.jsx";
-import { useState } from "react";
+import { useState } from 'react';
 
-// gameTurns -- from ActivePLayer
-function deriveActivePlayer(gameTurns) {
-  let currentPlayer = 'X';
-
-  if (gameTurns.length > 0 && gameTurns[[0].player === 'X']) {
-    currentPlayer = 'O';
-  }
-
-  return currentPlayer;
-}
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
-  const [gameTurns, setGameTurns] = useState([]);
-  // const [activePlayer, setActivePlayer] = useState('X');
+  const [selectedTopic, setSelectedTopic] = useState();
 
-  const activePlayer = deriveActivePlayer(gameTurns);
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+  }
 
-  const handleSelectSqaure = (rowIndex, colIndex) => {
-    setGameTurns(prevTurns => {
-      const currentPlayer = deriveActivePlayer(prevTurns);
+  console.log('APP COMPONENT EXECUTING');
 
-      const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
-        ...prevTurns
-      ];
+  let tabContent = <p>Please select a topic.</p>;
 
-      return updatedTurns;
-    });
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
-    <main>
-      <div id="game-container">
-
-        <ol id="players" className="highlight-player">
-          <Player initialName={'Player 1'} symbol={'X'}
-            isActive={activePlayer === 'X'}
-          />
-          <Player initialName={'Player 2'} symbol={'O'}
-            isActive={activePlayer === 'O'}
-          />
-        </ol>
-        <hr />
-        <GameBoard
-          onsSelectSqaure={handleSelectSqaure}
-          turns={gameTurns}
-        />
-      </div>
-
-      {/* <Log /> */}
-    </main>
-  )
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onSelect={() => handleSelect('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onSelect={() => handleSelect('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onSelect={() => handleSelect('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onSelect={() => handleSelect('state')}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<App />);
