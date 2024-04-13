0e8f89fd0c08f0e6064bd32433aa2710cf2df883
SynchroTek <satyajitnayak348@gmail.com>
2024-04-13 16:20:47 +0530

    basic player set

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import Player from "./components/Player"
 
 function App() {
-  
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
+
+  return (<main>
+    <div id="game-container">
+      <ol id="players">
+        <Player intialName="Player 1" symbol="X" />
+        <Player intialName="Player 2" symbol="O" />
+      </ol>
+
+      GAME BOARD
+    </div>
+
+    LOG
+  </main>
   )
 }
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+export default function Player({ intialName, symbol }) {
+    const [playerName, setPlayerName] = useState(intialName)
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = (e) => {
+        setIsEditing((editing) => !editing);
+    }
+
+    const handleChange = (e) => {
+        console.log(e);
+        setPlayerName(e.target.value);
+    }
+
+    let editablePlayerName = <span className='player-name'>{playerName}</span>;
+    // let btnCaption = "Edit";
+    if (isEditing) {
+        editablePlayerName = <input type="text" required
+            defaultValue={playerName} onChange={handleChange} />;
+        // btnCaption = "Save";
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+                <button onClick={handleEditClick}>
+                    {isEditing ? 'Save' : 'Edit'}
+                </button>
+            </span>
+        </li>
+    )
+}
