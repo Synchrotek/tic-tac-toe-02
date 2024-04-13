02caf10d15e24c8e0bf097c2cbd6446bc31ac593
ByteAstro <biswajitswain348@gmail.com>
2024-04-13 14:39:23 +0530

    project restructure

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 
-import App from "./App.jsx";
-import "./index.css";
+import App from './App.jsx';
+import './index.css';
 
-const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
